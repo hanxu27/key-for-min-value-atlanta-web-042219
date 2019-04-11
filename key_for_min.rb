4346bfd5578dc6300d00e32262fc97ca901a7e01
han xu <hanxu27@gmail.com>
2019-04-11 16:07:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,9 @@ def key_for_min_value(name_hash)
   output_key = hash.keys[0]
   output_value = hash.values[0]
   name_hash.each do |key, value|
-    output = key  
+    if value < output_value
+      output_key = key
+    end
+  end
+  output_key
 end
